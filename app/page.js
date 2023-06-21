3a51f71fc0ebdec56de24380bffff5c8595cdc9c
Minseok Song <minseoksong8@gmail.com>
2023-06-20 22:44:54 -0700

    completed browse by marine life section

--- app/page.js
+++ app/page.js
@@ -1,19 +1,36 @@
 import Banner from '@/components/Banner'
+import SmallCard from '@/components/SmallCard';
 import Image from 'next/image'
 
-export default function Home() {
+const sampleMarineLifeData = [{ "name": "Manta ray", "img": "/img/marineLifeIcons/manta.png" }, { "name": "Shark", "img": "/img/marineLifeIcons/shark.png" }, { "name": "Turtle", "img": "/img/marineLifeIcons/turtle.png" }, { "name": "Whale", "img": "/img/marineLifeIcons/whale.png" }, { "name": "Seahorse", "img": "/img/marineLifeIcons/seahorse.png" }]
+
+async function getMarineLife() {
+  const response = await fetch("https://mocki.io/v1/56e4ee98-ae64-47ab-9bc8-0a54de3b9c21");
+  return response.json();
+}
+
+
+export default async function Home() {
+  const marineLife = await getMarineLife();
   return (
     <main>
       <Banner />
-      <section>
-        <h2>Explore by marine life</h2>
-      </section>
-      <section>
-        <h2>Browse by region </h2>
-      </section>
-      <section>
-        <h2>Top 10 dive sites</h2>
-      </section>
+      <div className=' max-w-7xl mx-auto px-8 sm:px-16'>
+        <section className=' pt-6'>
+          <h2 className=' text-4xl font-semibold pb-5'>Browse by marine life</h2>
+          <h3 className='text-4xl'>
+            {sampleMarineLifeData?.map(({name, img}, id) => (
+            <SmallCard img={img} name={name} key={id} />
+          ))}
+          </h3>
+        </section>
+        <section>
+          <h2>Browse by region </h2>
+        </section>
+        <section>
+          <h2>Top 10 dive sites</h2>
+        </section>
+      </div>
     </main>
   )
 }
---
+++ components/SmallCard.js
@@ -0,0 +1,18 @@
+import Image from "next/image"
+
+function SmallCard({name, img}) {
+  return (
+    <div className="flex items-center m-2 mt-5 space-x-4 rounded-xl cursor-pointer hover:bg-gray-100 hover:scale-105 transition transform duration-200 ease-out">
+      {/* left */}
+      <div className="relative h-16 w-16">
+        <Image src={img} fill className="rounded-lg" alt="animal icon" />
+      </div>
+      {/* right */}
+      <div>
+        <h2 className=" text-lg">{name}</h2>
+      </div>
+    </div>
+  )
+}
+
+export default SmallCard
